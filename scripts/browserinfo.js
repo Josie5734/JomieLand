b807f3b819b8ca68b25a7d469b13980a796cd6b4
Josie5734 <armyjoe999@gmail.com>
2025-09-29 20:31:08 +0100

    experimented with some javascript for rotating images

--- scripts/browserinfo.js
+++ scripts/browserinfo.js
@@ -4,26 +4,24 @@ var detection2 = document.getElementById('detection2');
 
 // detect web browser
 if (navigator.userAgent.includes("Chrome")) {
-    detection.innerText = "you are using an inferior web browser. disgusting.";
+    detection.innerText = "you are using chrome";
 } else if (navigator.userAgent.includes("Vivaldi")) {
-    detection.innerText = "vivaldi? what the hell is a vivaldi?";
+    detection.innerText = "you are using vivaldi";
 } else if (navigator.userAgent.includes("Safari")) {
-    detection.innerText = "safari? you scumbag.";
-} else if (navigator.userAgent.includes("AppleWebKit")) {
-    detection.innerText = "you're using an apple product? what's wrong with you?";
+    detection.innerText = "you are using safari";
 } else if (navigator.userAgent.includes("Edge") || navigator.userAgent.includes("Edg")) {
-    detection.innerText = "living on the edge, huh? use a better web browser..";
+    detection.innerText = "you are using edge";
 } else if (navigator.userAgent.includes("Firefox") || navigator.userAgent.includes("Mozilla")) {
-    detection.innerText = "thanks for using firefox!";
+    detection.innerText = "you are using firefox";
 } else {
     detection.innerText = "error detecting web browser...";
 }
 
 // detect operating system
 if (navigator.userAgent.includes("Linux")) {
-    detection2.innerText = "thanks for using linux!";
+    detection2.innerText = "linux user detected";
 } else if (navigator.userAgent.includes("Android")) {
-    detection2.innerText = "android user detected...";
+    detection2.innerText = "android user detected";
 } else {
-    detection2.innerText = "please switch to linux...";
+    detection2.innerText = "youre not using android or linux :0";
 }
---
+++ scripts/imagerotate.js
@@ -0,0 +1,49 @@
+//rotated image function
+let jimmyinfo = document.getElementById("jimmyinfo");
+let jimmypic = document.getElementById("jimmypic");
+let jimmystyle = getComputedStyle(document.getElementById("jimmypic"));
+let jimmyangle = jimmystyle.getPropertyValue("rotate").substring(0,2);
+
+//add eventlisteners
+document.getElementById("jimmyleft").addEventListener("click", function() {rotateimage(jimmypic,"left");});
+document.getElementById("jimmyright").addEventListener("click", function() {rotateimage(jimmypic,"right");});
+document.getElementById("jimmyreset").addEventListener("click", function() { //reset function
+    jimmypic.style.rotate="0deg"; 
+    jimmyinfo.innerText = "jimmy is at 0 degrees";
+});
+
+jimmyinfo.innerText = "jimmy is at " + getdegrees() + " degrees";
+
+//get the current rotation in degrees as an integer
+function getdegrees() {
+    let degrees = getComputedStyle(document.getElementById("jimmypic")).getPropertyValue("rotate"); //get the property value
+
+    let numberdegrees; //hold the integer value
+
+    if (degrees.length == 4) { //convert it to an integer by removing the "deg" and converting the remaining number
+        numberdegrees = Number(degrees.substring(0,1));
+    } else if (degrees.length == 5) {
+        numberdegrees = Number(degrees.substring(0,2));
+    } else if (degrees.length == 6) {
+        numberdegrees = Number(degrees.substring(0,3));
+    }
+    
+    return numberdegrees
+}
+
+//changes the "rotate" property value of the given image
+function rotateimage(image,direction) {
+    let rotatevalue = 10;   //how much to rotate by
+    if (direction == "left") { //change to negative increase to go left
+        rotatevalue = -rotatevalue;
+    } 
+
+    let current = getdegrees(); //current value
+
+    let newvalue = current + rotatevalue; //new value
+
+    image.style.rotate=newvalue+"deg"; //change value
+
+    jimmyinfo.innerText = "jimmy is at " + newvalue + " degrees"; //print the new value in the textbox below (temporary, will be removed in any real application) 
+
+}
